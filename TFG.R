c80b745c7bcf06c837fe1cb7b27c87e41faba538
rocioagmi <101205869+rocioagmi@users.noreply.github.com>
2024-10-13 22:58:06 +0200

    Initial commit

--- TFG.R
+++ TFG.R
@@ -1,42 +1,44 @@
 # BIOCONDUCTOR
-# if (!require("BiocManager", quietly = TRUE))
-#  install.packages("BiocManager")
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
 
-# BiocManager::install(c("GenomicFeatures", "AnnotationDbi"))
+BiocManager::install(c("GenomicFeatures", "AnnotationDbi"))
 
 # Biostrings
-# BiocManager::install("Biostrings", force = TRUE)
+BiocManager::install("Biostrings", force = TRUE)
 
 # DADA2
-# BiocManager::install("dada2", force = TRUE)
+BiocManager::install("dada2", force = TRUE)
 
 # phyloseq
-# BiocManager::install("phyloseq")
+BiocManager::install("phyloseq")
 
 # DESeq2
-# BiocManager::install("DESeq2")
+BiocManager::install("DESeq2")
 
 # microbiome
-# BiocManager::install("microbiome")
+BiocManager::install("microbiome")
 
 # qiime2R
-# if (!requireNamespace("devtools", quietly = TRUE)){install.packages("devtools")}
-# devtools::install_github("jbisanz/qiime2R")
+if (!requireNamespace("devtools", quietly = TRUE)){install.packages("devtools")}
+  devtools::install_github("jbisanz/qiime2R")
 
 # ANCOMBC
-# BiocManager::install("ANCOMBC") 
+BiocManager::install("ANCOMBC") 
 
 # microbiotaProcess
-# BiocManager::install("MicrobiotaProcess")
+BiocManager::install("MicrobiotaProcess")
 
 # mongolite
 install.packages("mongolite")
 
+# fastqcr
+install.packages("fastqcr")
 
-# Descarga automatizada de los datos de la BBDD
 
 library(readr)
 library(dplyr)
+library(Biostrings)
 library(mongolite)
 
 tabla <- read_delim("C:/ANTIGUA_D/TodoTFG/filereport_read_run_PRJEB34168_tsv.tsv", col_names = TRUE, delim ="\t")
@@ -46,10 +48,12 @@ tabla_ordenada <- tabla[order(tabla$submitted_ftp), ]
 head(tabla_ordenada)
 
 enlaces <- strsplit(tabla_ordenada$submitted_ftp, ";")
-# print(enlaces[[65]][1])
+print(enlaces)
 
-# Revisarlo da error porque no encuentra el servidor
-for (i in 1:length(enlaces)){
-  flt <- mongo("flights")
-  muestras <- flt$import(gzcon(curl::curl(enlaces[i])))
+# AUTOMATIZACIÓN DE LA DESCARGA DE LAS MUESTRAS DEL ENA
+for (enl in enlaces){
+  for (i in enl){
+    destino <- strsplit(i, "/")[[1]]
+    download.file(i,destino[length(destino)])
+  }
 }
