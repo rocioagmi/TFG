34b6fb3f23bf5c86a59fdff4f8075023be627a03
rocioagmi <101205869+rocioagmi@users.noreply.github.com>
2024-10-16 13:10:34 +0200

    Implementación de la función descargas_ENA

--- FUNC/Descargas_ENA.R
+++ FUNC/Descargas_ENA.R
@@ -1,1 +1,25 @@
-# DESCARGAS_ENA
+
+# AUTOMATIZACIÓN DE LA DESCARGA DE LAS MUESTRAS DEL ENA
+
+library(readr)
+library(dplyr)
+library(Biostrings)
+library(svDialogs)
+
+#   DESCARGA DEL TSV 
+
+#   DESCARGA DE SECUENCIAS
+directorio <- dlgInput(message = "Introduzca el directorio al tsv: ")$res
+
+tabla <- read_delim(directorio, col_names = TRUE, delim ="\t")
+
+tabla_ordenada <- tabla[order(tabla$submitted_ftp), ]
+
+enlaces <- strsplit(tabla_ordenada$submitted_ftp, ";")
+
+for (enl in enlaces){
+  for (i in enl){
+    destino <- strsplit(i, "/")[[1]]
+    download.file(i,destino[length(destino)])
+  }
+}
