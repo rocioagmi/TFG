87e484f8e65c128b7489d6f31fa24597f62a4d24
rocioagmi <101205869+rocioagmi@users.noreply.github.com>
2024-10-16 13:58:56 +0200

    Borramos los datos para volver a comprobar el código

--- FUNC/Descargas_ENA.R
+++ FUNC/Descargas_ENA.R
@@ -6,29 +6,27 @@ library(dplyr)
 library(svDialogs)
 library(TAF)
 
-#   DESCARGA DEL TSV 
-nAcceso <- dlgInput(message = "Introduzca el número de acceso al proyecto ENA: ")$res
 
-enlaceTSV <- paste("https://www.ebi.ac.uk/ena/portal/api/filereport?accession=","&result=read_run&fields=study_accession,sample_accession,experiment_accession,run_accession,tax_id,scientific_name,fastq_bytes,fastq_ftp,submitted_bytes,submitted_ftp,sra_bytes,sra_ftp,bam_ftp,bam_bytes&format=tsv&download=true&limit=0", sep = nAcceso)
+descargas_ENA <- function(nAcceso){
+#nAcceso <- dlgInput(message = "Introduzca el número de acceso al proyecto ENA: ")$res
 
-directorio <- mkdir("INPUT/DATA")
+  # DESCARGA DEL TSV
+  enlaceTSV <- paste("https://www.ebi.ac.uk/ena/portal/api/filereport?accession=","&result=read_run&fields=study_accession,sample_accession,experiment_accession,run_accession,tax_id,scientific_name,fastq_bytes,fastq_ftp,submitted_bytes,submitted_ftp,sra_bytes,sra_ftp,bam_ftp,bam_bytes&format=tsv&download=true&limit=0", sep = nAcceso)
+  directorio <- mkdir("INPUT/DATA")
+  directorioTSV <- paste("INPUT/DATA/filereport_read_run_","_tsv.tsv",sep = nAcceso)
+  tsv <- download.file(enlaceTSV, directorioTSV)
 
-directorioTSV <- paste("INPUT/DATA/filereport_read_run_","_tsv.tsv",sep = nAcceso)
+  # DESCARGA DE LAS MUESTRAS
+  tabla <- read_delim(directorioTSV, col_names = TRUE, delim ="\t")
+  tabla_ordenada <- tabla[order(tabla$submitted_ftp), ]
+  enlaces <- strsplit(tabla_ordenada$submitted_ftp, ";")
 
-tsv <- download.file(enlaceTSV, directorioTSV)
-
-
-#   DESCARGA DE SECUENCIAS
-tabla <- read_delim(directorioTSV, col_names = TRUE, delim ="\t")
-
-tabla_ordenada <- tabla[order(tabla$submitted_ftp), ]
-
-enlaces <- strsplit(tabla_ordenada$submitted_ftp, ";")
-
-for (enl in enlaces){
-  for (i in enl){
-    destino <- strsplit(i, "/")[[1]]
-    directorioEnl <- paste("INPUT/DATA/",destino[length(destino)])
-    download.file(i,directorioEnl)
+  for (enl in enlaces){
+    for (i in enl){
+      destino <- strsplit(i, "/")[[1]]
+      directorioEnl <- paste("INPUT/DATA/",destino[length(destino)])
+      download.file(i,directorioEnl)
+    }
   }
+  
 }
