2194341420d1a6813b0a8adcdb6c401903412920
rocioagmi <101205869+rocioagmi@users.noreply.github.com>
2024-11-26 12:43:32 +0100

    último cambio

--- FUNC/FiltrarMuestras.R
+++ FUNC/FiltrarMuestras.R
@@ -8,9 +8,9 @@ filtrarMuestras <- function(R1, R2){
   nombres_R1 <- paste("INPUT/DATA", strsplit(basename(R1), "/M"),sep = "/FILTRADAS/")
   nombres_R2 <- paste("INPUT/DATA", strsplit(basename(R2), "/M"),sep = "/FILTRADAS/")
   
-  #filterAndTrim(R1, nombres_R1, R2, nombres_R2, truncLen = c(240,220),
-  #               trimLeft = c(15, 15), maxN = 0, maxEE = c(2,2), truncQ = 10, 
-  #               rm.phix = TRUE, compress = TRUE, multithread = FALSE)
+  filterAndTrim(R1, nombres_R1, R2, nombres_R2, truncLen = c(240,220),
+                 trimLeft = c(15, 15), maxN = 0, maxEE = c(2,2), truncQ = 10, 
+                 rm.phix = TRUE, compress = TRUE, multithread = FALSE)
 
   return(list(nombres_R1 = nombres_R1, nombres_R2 = nombres_R2))
 }
