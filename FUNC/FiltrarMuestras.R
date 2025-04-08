1837fa96b15a92f2b6a22c9b07b654de3ec06981
rocioagmi <101205869+rocioagmi@users.noreply.github.com>
2025-04-08 17:11:41 +0200

    Comparación de los report de las dos funciones

--- FUNC/FiltrarMuestras.R
+++ FUNC/FiltrarMuestras.R
@@ -5,8 +5,11 @@ library(ShortRead)
 library(dada2)
 
 filtrarMuestras <- function(R1, R2){
-  nombres_R1 <- paste("INPUT/DATA", strsplit(basename(R1), "/M"),sep = "/FILTRADAS/")
-  nombres_R2 <- paste("INPUT/DATA", strsplit(basename(R2), "/M"),sep = "/FILTRADAS/")
+  #nombres_R1 <- paste("INPUT/DATA", strsplit(basename(R1), "/M"),sep = "/FILTRADAS/")
+  #nombres_R2 <- paste("INPUT/DATA", strsplit(basename(R2), "/M"),sep = "/FILTRADAS/")
+  
+  nombres_R1 <- file.path("INPUT/DATA/FILTRADAS", basename(R1))
+  nombres_R2 <- file.path("INPUT/DATA/FILTRADAS", basename(R2))
   
   filterAndTrim(R1, nombres_R1, R2, nombres_R2, truncLen = c(240,200),
                  trimLeft = c(15, 15), maxN = 0, maxEE = c(2,2), truncQ = 10, 
