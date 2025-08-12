5a65b92d52c1dff44c8de77e84c9529f91d4c05b
rocioagmi <101205869+rocioagmi@users.noreply.github.com>
2025-08-12 23:25:50 +0200

    Probando informe de calidad con Rqc

--- FUNC/InformeCalidad.R
+++ FUNC/InformeCalidad.R
@@ -1,4 +1,8 @@
-informeCalidad <- function(directorioMuestras){
+informeCalidadSR <- function(directorioMuestras){
   qaSummary <- qa(directorioMuestras, type = "fastq")
   browseURL(report(qaSummary)) 
+}
+
+informeCalidadR <- function(directorioMuestras){
+  qaSummary <- rqc(path = directorioMuestras, pattern = "fastq.gz", openBrowser = TRUE)
 }
