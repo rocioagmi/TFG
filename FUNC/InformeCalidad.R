bbdc4ba8c65ac04adccaa121532a800d9d8cec73
rocioagmi <101205869+rocioagmi@users.noreply.github.com>
2025-08-18 17:54:46 +0200

    Cambiar nombre de la función

--- FUNC/InformeCalidad.R
+++ FUNC/InformeCalidad.R
@@ -1,4 +1,4 @@
-informeCalidadSR <- function(directorioMuestras){
+informeCalidad <- function(directorioMuestras){
   qaSummary <- qa(directorioMuestras, type = "fastq")
   report(qaSummary, dest = "INPUT/DATA/REPORT", type = "html")
   browseURL(file.path("INPUT/DATA/REPORT", "index.html")) 
