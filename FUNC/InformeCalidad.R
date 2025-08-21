e4ba77c565a8010323f035fda09052bdbf1753a7
rocioagmi <101205869+rocioagmi@users.noreply.github.com>
2025-08-21 08:49:04 +0200

    Creamos nuevo directorio

--- FUNC/InformeCalidad.R
+++ FUNC/InformeCalidad.R
@@ -1,5 +1,6 @@
 informeCalidad <- function(directorioMuestras){
   qaSummary <- qa(directorioMuestras, type = "fastq")
+  direcrtorio <- mkdir("OUTPUT/REPORT")
   report(qaSummary, dest = "OUTPUT/REPORT", type = "html")
   browseURL(file.path("OUTPUT/REPORT", "index.html")) 
 }
