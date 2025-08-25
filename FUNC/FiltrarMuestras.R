0dceb6c3da5233aed33ca1abf6728b3ef533d00b
rocioagmi <101205869+rocioagmi@users.noreply.github.com>
2025-08-25 11:35:40 +0200

    Eliminar filtrados duplicados

--- FUNC/FiltrarMuestras.R
+++ FUNC/FiltrarMuestras.R
@@ -9,11 +9,9 @@ filtrarMuestras <- function(R1, R2){
   nombres_R1 <- file.path("OUTPUT/FILTRADO", basename(R1))
   nombres_R2 <- file.path("OUTPUT/FILTRADO", basename(R2))
   
-  out <- filterAndTrim(R1, nombres_R1, R2, nombres_R2, truncLen = c(240,160),
+  filterAndTrim(R1, nombres_R1, R2, nombres_R2, truncLen = c(240,160),
                 trimLeft = c(10, 10), maxN = 0, maxEE = c(2,5), truncQ = 2, 
                 rm.phix = TRUE, compress = TRUE, multithread = FALSE)
-  
-  write.csv(out, "OUTPUT/FILTRADO/filter_summary.csv", row.names = FALSE)
   
   return(list(nombres_R1 = nombres_R1, nombres_R2 = nombres_R2))
 }
