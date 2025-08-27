cd75a571ecd208e2147205ee5401f5813240c5d8
rocioagmi <101205869+rocioagmi@users.noreply.github.com>
2025-08-27 16:57:04 +0200

    Corrección de la función de filtrado

--- FUNC/FiltrarMuestras.R
+++ FUNC/FiltrarMuestras.R
@@ -9,7 +9,7 @@ filtrarMuestras <- function(R1, R2){
   nombres_R1 <- file.path("OUTPUT/FILTRADO", basename(R1))
   nombres_R2 <- file.path("OUTPUT/FILTRADO", basename(R2))
   
-  filterAndTrim(R1, nombres_R1, R2, nombres_R2, truncLen = c(240,160),
+  filterAndTrim(R1, nombres_R1, R2, nombres_R2, truncLen = c(230,170),
                 trimLeft = c(10, 10), maxN = 0, maxEE = c(2,5), truncQ = 2, 
                 rm.phix = TRUE, compress = TRUE, multithread = FALSE)
   
