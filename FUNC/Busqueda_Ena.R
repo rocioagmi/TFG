a8d803870135a491066b054014a107b7a5c0b9f7
rocioagmi <101205869+rocioagmi@users.noreply.github.com>
2025-11-06 01:33:55 +0100

    función búsqueda

--- FUNC/Busqueda_Ena.R
+++ FUNC/Busqueda_Ena.R
@@ -1,7 +1,7 @@
-busquedaENA <- function(dominio, query, fields, limit = 100) {
+busquedaENA <- function(dominio, query, fields, limit = 1000) {
   url <- paste0("https://www.ebi.ac.uk/ebisearch/ws/rest/", dominio)
   
-  query <- gsub('"', '', query)
+  # query <- gsub('"', '', query)
   fields <- gsub(",\\s*", ",", fields)
   
   parametros <- list(
@@ -27,30 +27,32 @@ busquedaENA <- function(dominio, query, fields, limit = 100) {
   dataJson <- fromJSON(contenidoRespuesta, flatten = TRUE)
   
   if (dataJson$hitCount == 0) {
-    print("No se encontraron resultados para esta consulta.")
+    print(paste("No se encontraron resultados para esta consulta:", respuesta$url))
   }
   
-  resultadosDF <- dataJson$entries
+  resultadosDF <- as.data.frame(dataJson$entries)
   return(resultadosDF)
 }
 
+
 construirConsulta <- function(limit = 1000) {
-  dominio <- dlgInput(message = "Introduzca el dominio de búsqueda (biosamples, embl):")$res
+  dominio <- dlgInput(message = "Introduzca el dominio de búsqueda (biosamples, sra-sample):")$res
   if (!is.character(dominio) || length(dominio) == 0) {
-    print("Operación cancelada por el usuario (dominio).")
+    print("Operación (dominio) cancelada por el usuario.")
     return(NULL)
   }
   
   query <- dlgInput(message = "Ingrese un término de búsqueda:")$res
   if (!is.character(query) || length(query) == 0) {
-    print("Operación cancelada por el usuario (query).")
+    print("Operación (query) cancelada por el usuario.")
     return(NULL)
   }
   
   dominio <- trimws(dominio)
   query <- trimws(query)
   
-  fields <- "id,acc,description,sample_title"
+  # "id,acc,description,sample_title"
+  fields <- "sample_accession,study_accession,description,disease,subject"
   
   muestrasDF <- busquedaENA(dominio = dominio, query = query, fields = fields, limit = limit)
   
